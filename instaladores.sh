b9ed0a39d2e9fb4696d2bb9fe87de8bcf7f13e61
EKolling <82099922+EKolling@users.noreply.github.com>
2023-08-09 12:08:37 -0300

    Create instaladores.sh

---
+++ instaladores.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Verifica se o script está sendo executado como root (sudo)
+if [ "$EUID" -ne 0 ]; then
+  echo "Por favor, execute este script com privilégios de superusuário (sudo)."
+  exit 1
+fi
+
+# Atualiza a lista de pacotes disponíveis e instala o pip3, caso ainda não esteja instalado
+apt update
+apt install -y python3-pip
+
+# Instala as dependências
+pip3 install python-dotenv python-dateutil boto3 python-crontab
+
+# Atualiza o AWS CLI, caso já esteja instalado
+pip3 install --upgrade awscli
+
+echo "Instalação concluída!"
